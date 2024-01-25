ed4719a301a606dc7940842304f93ac995e0e241
valeriocoletta <157354302+valeriocoletta@users.noreply.github.com>
2024-01-25 10:36:16 +0000

    c

--- problema6.c
+++ problema6.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
-    int numero;
-    printf("inserisci un numero\n");
-    scanf("%d", &numero);
-    if(numero)
+    int a;
+    int b;
+    int c;
+    int x1;
+    int x2;
+    int delta;
+    printf("inserisci tre valori\n");
+    scanf("%d %d %d", &a, &b, &c);
+    delta = b * b - (4 * a *c);
+    printf("delta è uguale a:%d\n", delta);
+    x1 = ( -b + sqrt( delta))/2 * a;
+    x2 = (-b - sqrt( delta))/2 * a;
+    if(delta > 0)
+    {
+        printf("i risultati sono:%d %d\n", x1, x2);
+    }
+    else if(delta == 0)
+    {
+        x1==x2;
+        printf(" i risultati coincidono:%d %d\n", x1, x2);
+    }
+    else if(delta < 0)
+    {
+        printf("l'equazione non ammette soluzioni reali\n");
+    }
+    
+
     return(0);
 }
